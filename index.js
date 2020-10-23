d950d7b7a7252f257e9c9b20ff2e723a66c7d6dd
techurgency <68974295+techurgency@users.noreply.github.com>
2020-10-23 15:28:05 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes");
+
+var supervillains = require("supervillains");
+
+var he = superheroes.random();
+
+console.log(superheroes.random() + " vs " + supervillains.random());
